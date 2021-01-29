a0902dbba4b1b40e471ba6f1a156209a73b435b3
MuhammadHazrat <221441166@formanite.fccollege.edu.pk>
2021-01-29 05:07:54 -0800

    Initial commit

---
+++ app/src/main/java/com/example/messagingapp/PhoneVerification.java
@@ -0,0 +1,19 @@
+package com.example.messagingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhoneVerification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_verification);
+
+        getSupportActionBar().hide();
+
+
+
+    }
+}
